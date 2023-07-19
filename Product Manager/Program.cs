64eb712457abcd417525ab3029c25cb2c9b11c3d
kyozun <daicacuong0311@gmail.com>
2023-07-19 09:20:24 +0700

    Hello c#

---
+++ Product Manager/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+bool isSelected = false;
+while (!isSelected) {
+    Console.WriteLine("1. Add product records");
+    Console.WriteLine("2. Display product records");
+    Console.WriteLine("3. Delete product by Id");
+    Console.WriteLine("4. Exit");
+}
+
